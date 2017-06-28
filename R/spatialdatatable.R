e0b5aa97241866034f5e2d80d77c9ed3e235d1a2
David Cooley <dcooley@symbolix.com.au>
2017-06-28 15:59:57 +1000

    trying to subset

--- R/spatialdatatable.R
+++ R/spatialdatatable.R
@@ -15,38 +15,43 @@ print.spatialdatatable <- function(x, ...){
 	NextMethod()
 }
 
-# poly_column <- "polyline"
-# attr(spdt_melbourne, "spdt_polyline") <- poly_column
-#
-# attributes(spdt_melbourne$polyline) <- NULL
-# str(spdt_melbourne)
-#
-# poly_col <- attr(spdt_melbourne, "spdt_polyline")
 
-# as.test <- function(x) {
-# 	class(x) <- c('test', class(x))
-# 	attr(x, "test_col") <- "y"
-# 	x
-# }
-#
-# print.test <- function(x) {
-# 	testCol <- names(x)[names(x) %in% attr(x, "test_col")]
-# 	if(!is.null(testCol)){
-# 		x[['y']] <- "more tests"
-# 	}
-# 	NextMethod()
-# }
-#
-# find.test.col <- function(x) UseMethod("find_test_col")
-#
-# find_test_col.test <- function(x){
-# 	names(x)[names(x) %in% attr(x, "test_col")]
-# }
-#
-# a <- data.table(x = 1:5, y = 1:5)
-# b <- as.test(a)
-#
-# b
-# str(b)
-# b[1:2, ]
+#' #' @export
+#' `[.spatialdatatable` <- function(x, ...){
+#'
+#' 	## need to keep the polyline attribute if it exists
+#' 	print("subsetting")
+#' 	NextMethod()
+#' }
+#'
+#'
+#' as.test <- function(x) {
+#' 	class(x) <- c('test', class(x))
+#' 	attr(x, "test_col") <- "y"
+#' 	attr(x[["y"]], "test_col_") <- "the column"
+#' 	x
+#' }
+#'
+#' print.test <- function(x) {
+#' 	testCol <- names(x)[names(x) %in% attr(x, "test_col")]
+#' 	print( paste0("testCol: ", testCol))
+#' 	if(!is.null(testCol)){
+#' 		x[['y']] <- "more tests"
+#' 	}
+#' 	NextMethod()
+#' }
+#'
+#' find.test.col <- function(x) UseMethod("find_test_col")
+#'
+#' find_test_col.test <- function(x){
+#' 	names(x)[names(x) %in% attr(x, "test_col")]
+#' }
+#'
+#' a <- data.table(x = 1:5, y = 1:5)
+#' b <- as.test(a)
+#'
+#' b
+#' str(b)
+#' b[1:2, ]
+#' str(b[1:2, ])
 
